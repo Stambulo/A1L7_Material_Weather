bf22f93684ebdc261bce566943b92f0d6cb2ddea
Anatolii Stambulo <stambulo@yandex.ru>
2020-06-09 14:47:12 +0300

    Initial commit

---
+++ app/src/main/java/com/stambulo/materialdesign/MainActivity.java
@@ -0,0 +1,29 @@
+package com.stambulo.materialdesign;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.button.MaterialButton;
+
+public class MainActivity extends AppCompatActivity {
+    MaterialButton loginButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        loginButton = findViewById(R.id.loginButton);
+        setOnBtnClickListener();
+    }
+
+    private void setOnBtnClickListener() {
+        //Откроем вторую активи при нажатии на кнопку
+        loginButton.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, WeatherActivity.class);
+            startActivity(intent);
+        });
+    }
+}
---
+++ app/src/main/java/com/stambulo/materialdesign/WeatherActivity.java
@@ -0,0 +1,6 @@
+package com.stambulo.materialdesign;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class WeatherActivity extends AppCompatActivity {
+}
